594e20ba8ee34056f00dde57a4b85aff107954e6
Linas <66268230+linas92@users.noreply.github.com>
2021-04-28 19:43:00 +0300

    Add project files.

---
+++ 2021_04_28_LAST_ONE/ManoKlase.cs
@@ -0,0 +1,7 @@
+﻿namespace _2021_04_28_LAST_ONE
+{
+    public class ManoKlase 
+    {
+        public string Raudona { get; set; }
+    }
+}
---
+++ 2021_04_28_LAST_ONE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _2021_04_28_LAST_ONE
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            Console.ReadKey();
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Goodbye World!");
+        }
+    }
+}
+/*          1. Sukurti klasę Spalva. Spalva paprastai aprašoma RGB kodu skaičiais nuo 0 iki 255. Ty maišant raudoną, žalią ir mėlyną
+                - sukurti reikiamus property, kuris saugotu spalvos informaciją
+                - sukurti konstruktorių kuris priima raudoną, žalią ar mėlyną spalvas
+                - sukurti konstruktorių kuris jei nepaduodama spalvos - priskiria default (balta)
+                - sukurti metodus kiekvienos spalvos kodo keitimui
+                - sukurti metodą kuris gąžintu vidutinę RGB properčių reikšmę (grayscale value)
+            2. Sukurti klasę Kamuolys, 
+                - kurioje turi būti kompozicija į klasę Spalva
+                - kamuolys turi dydį
+                - kamuolys turi sekti kiek kartų buvo metamas
+                - sukurti konstruktorių(ar konstruktorius) su parametrais kurių gali reikėti valeidžiai klasei sukonstruoti
+                - sukurti metodą Mesti, kuris prideda 1 prie metimų kiekio
+                - sukurti metodą Susprogdinti, kuris dydį sumažina iki 0
+*/
